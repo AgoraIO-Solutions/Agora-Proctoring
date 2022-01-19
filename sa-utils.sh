da77752b23e5f389b06db1eb271f43ae9c1aa7ae
Ben Weekes <benweekes73@gmail.com>
2022-01-19 09:23:55 +0000

    script to take and deploy latest server on server

---
+++ sa-utils.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ~
+sudo rm -rf Agora-Proctoring
+git clone https://github.com/AgoraIO-Solutions/Agora-Proctoring
+rm -rf ~/Agora-Proctoring/.git
+cd ~/Agora-Proctoring
+npm install
+rm ~/Agora-Proctoring/pack*
+cd ~/Agora-Proctoring/sh
+./run.sh
