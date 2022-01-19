3ee744e5d5e42b88203b51d18da005987bedcd37
Ben Weekes <benweekes73@gmail.com>
2022-01-19 11:55:48 +0000

    improve sh

--- sa-utils.sh
+++ sa-utils.sh
@@ -5,6 +5,6 @@ git clone https://github.com/AgoraIO-Solutions/Agora-Proctoring
 rm -rf ~/Agora-Proctoring/.git
 cd ~/Agora-Proctoring
 npm install
-rm ~/Agora-Proctoring/pack*
+#rm ~/Agora-Proctoring/pack*
 cd ~/Agora-Proctoring/sh
 ./run.sh
